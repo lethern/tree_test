f9260fd71c6ab08925a4b88f5676c9f40fec964a
lethern <lethern@o2.pl>
2018-12-13 03:13:35 +0100

    test: draw a rectangle

--- project_tree/wwwroot/JS/tree.js
+++ project_tree/wwwroot/JS/tree.js
@@ -4,15 +4,36 @@ function onLoad() {
 
 	CONSOLE.init_defaults();
 
+	CONSOLE.write('Tree init', CONSOLE.INFO);
+
 	test_elem = document.createTextNode('test');
 	document.getElementById('main_div').appendChild(test_elem);
 
 	let stage = new Kinetic.Stage({
 		container: 'render_div',
+		
 		width: 900,
-		height: 0,
+		height: 500,
 	});
-	let layer = new Kinetic.Layer();
+	let layer = new Kinetic.Layer({
+		x: 10,
+		y: 50,
+	});
+	stage.add(layer);
+
+	let x = 100;
+	let y = 100;
+
+	var rect = new Kinetic.Rect({
+		'x': x,
+		'y': y,
+		'width': 100,
+		'height': 50,
+		'fill': '#440',//class_color[system.class],
+		'stroke': '#ccc',
+		'strokeWidth': 2,
+	});
+	layer.add(rect);
 };
 
 window.addEventListener('load', onLoad);
